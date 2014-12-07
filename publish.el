eb8e7b3cb133349eb5bdcbd801dae9740c6c26e8
Felix Geller <fgeller@gmail.com>
2014-12-07 20:39:25 +1300

    Update publish script.

--- publish.el
+++ publish.el
@@ -1,4 +1,16 @@
-(require 'org-publish)
+(use-package uuid :ensure uuid)
+
+(defun insert-iso-timestamp ()
+  (interactive)
+  (let ((saved-tz (getenv "TZ")))
+    (setenv "TZ" "Etc/UTC")
+    (let ((date (with-temp-buffer
+		  (call-process "gdate" nil (current-buffer) nil "--iso-8601=seconds")
+		  (delete-backward-char 1)
+		  (buffer-substring (point-min) (point-max)))))
+      (setenv "TZ" saved-tz)
+      date)))
+
 (setq org-publish-project-alist
       '(("beltmogul-notes"
          :base-directory "~/blog/orgs/"
@@ -25,4 +37,13 @@
     (org-publish-project "beltmogul")
     (compile "rsync -azv public_html hipp0:~/beltmogul.me/")))
 
-(publish-beltmogul)
+(defun publish-beltmogul-locally ()
+  (interactive)
+  (let ((default-directory "~/blog/"))
+    (org-publish-project "beltmogul")))
+
+(let ((org-html-postamble-format
+       '(("en" "<div class=\"footer\"><a href=\"/\">belt mogul</a></div>"))))
+  (publish-beltmogul-locally))
+
+(publish-beltmogul-locally)
